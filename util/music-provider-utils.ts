a99f7e5bba39242dc083a6367e7b7ac5b2134bc0
janne6565 <jabbekeipert@gmail.com>
2026-02-10 01:18:05 +0100

    refactor: simplify openLink function and update `.idea` workspace configuration

--- util/music-provider-utils.ts
+++ util/music-provider-utils.ts
@@ -1,10 +1,5 @@
-import { Linking } from "react-native";
+import {Linking} from "react-native";
 
 export const openLink = async (link: string) => {
-  const supported = await Linking.canOpenURL(link);
-  if (supported) {
-    await Linking.openURL(link);
-  } else {
-    console.warn("Apple Music cannot be opened");
-  }
+  await Linking.openURL(link);
 };
